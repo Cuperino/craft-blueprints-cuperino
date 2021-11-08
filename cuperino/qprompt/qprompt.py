01c9e983c597bf89aa545049469a6c12c1bc65e5
Javier O. Cordero Pérez <javiercorderoperez@gmail.com>
2021-11-07 23:37:41 -0400

    An attempt to fix missing icon on Windows installer and reduce install size.

--- cuperino/qprompt/qprompt.py
+++ cuperino/qprompt/qprompt.py
@@ -39,4 +39,31 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self):
         CMakePackageBase.__init__(self)
-        self.defines["shortcuts"] = [{"name" : "QPrompt", "target":"bin/qprompt.exe", "description" : "Personal Teleprompter Software"}]
+        if CraftCore.compiler.isMacOS:
+            # Add APPLE_IN_APP_BUNDLE flag to enable private DBus, when packing by Craft
+            self.subinfo.options.configure.args += " -DAPPLE_IN_APP_BUNDLE=ON"
+        elif CraftCore.compiler.isWindows:
+            self.defines["shortcuts"] = [{"name" : "QPrompt", "target":"bin/qprompt.exe", "description" : "Personal Teleprompter Software"}]
+
+    def createPackage(self):
+        self.blacklist_file.append(os.path.join(self.packageDir(), "blacklist.txt"))
+        if CraftCore.compiler.isMacOS:
+            self.blacklist_file.append(os.path.join(self.packageDir(), 'blacklist_mac.txt'))
+
+        self.defines["website"] = "https://qprompt.app/"
+        self.defines["company"] = "Javier O. Cordero Pérez"
+        self.defines["productname"] = "QPrompt"
+        self.defines["appname"] = "qprompt"
+        self.defines["license"] = os.path.join(self.sourceDir(), "COPYING")
+        self.defines["executable"] = r"bin/qprompt.exe"
+
+        self.defines["icon"] = os.path.join(self.sourceDir(), "src", "icons", "qprompt.ico")
+        self.defines["icon_png"] = os.path.join(self.packageDir(), ".assets", "150-apps-com.cuperino.qprompt.png")
+        self.defines["icon_png_44"] = os.path.join(self.packageDir(), ".assets", "44-apps-com.cuperino.qprompt.png")
+        self.defines["icon_png_310x310"] = os.path.join(self.packageDir(), ".assets", "310-apps-com.cuperino.qprompt.png")
+
+        if isinstance(self, AppxPackager):
+            self.defines["display_name"] = "QPrompt"
+
+        self.ignoredPackages.append("binary/mysql")
+        return TypePackager.createPackage(self)
