6995d110a94da8989c74a73ef9d3cac59515bd93
Javier O. Cordero <javiercorderoperez@gmail.com>
2022-06-22 13:34:31 -0400

    Fix: Added missing dependency.

--- cuperino/clipboardinspector/clipboardinspector.py
+++ cuperino/clipboardinspector/clipboardinspector.py
@@ -32,6 +32,7 @@ class subinfo(info.infoclass):
             self.runtimeDependencies["kde/frameworks/tier1/breeze-icons"] = None 
         self.runtimeDependencies["libs/qt5/qtquickcontrols"] = None
         self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = None
+        self.runtimeDependencies["kde/frameworks/tier1/kcoreaddons"] = None
         self.runtimeDependencies["kde/frameworks/tier1/ki18n"] = None
         self.runtimeDependencies["kde/frameworks/tier1/kirigami"] = None
         self.runtimeDependencies["kde/frameworks/tier1/syntax-highlighting"] = None
